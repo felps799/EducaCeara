b1c2c48c84b77f8a9a413b54985e18c6c4584e39
Rian Eduardo <166628665+Ri4nedu@users.noreply.github.com>
2026-08-13 11:29:55 -0300

    Atualiza CORS para domínio do EducaCeará

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ ALLOWED_ORIGINS = [
     "https://educaceara.infinityfreeapp.com",
     "https://projeto2.fwh.is",
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://exemplo.infinityfree.me"
 ]
 
 app.add_middleware(
